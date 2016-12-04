36ba43bf4ce0ee872ed1dbf23f59a3bc4417dde7
bigbigda <iagp1991@163.com>
2016-12-04 23:04:58 +0800

    file change

--- basic_common_operation.py
+++ basic_common_operation.py
@@ -2,23 +2,23 @@ import numpy as np
 
 
 def read_data():
-    # train_data = np.genfromtxt('E:\\DataSet\\Letter\\train.txt', delimiter=',')
-    # test_data = np.genfromtxt('E:\\DataSet\\Letter\\test.txt', delimiter=',')
-    #
-    # train_label = train_data[:, 0].copy()
-    # train_feature = train_data[:, 1:17]
-    #
-    # test_label = test_data[:, 0].copy()
-    # test_feature = test_data[:, 1:17]
-
-    train_data = np.genfromtxt('E:\\DataSet\\opt\\train.txt', delimiter=',')
-    test_data = np.genfromtxt('E:\\DataSet\\opt\\test.txt', delimiter=',')
+    train_data = np.genfromtxt('E:\\DataSet\\Letter\\train.txt', delimiter=',')
+    test_data = np.genfromtxt('E:\\DataSet\\Letter\\test.txt', delimiter=',')
 
     train_label = train_data[:, 0].copy()
-    train_feature = train_data[:, 1:65]
+    train_feature = train_data[:, 1:17]
 
     test_label = test_data[:, 0].copy()
-    test_feature = test_data[:, 1:65]
+    test_feature = test_data[:, 1:17]
+
+    # train_data = np.genfromtxt('E:\\DataSet\\opt\\train.txt', delimiter=',')
+    # test_data = np.genfromtxt('E:\\DataSet\\opt\\test.txt', delimiter=',')
+    #
+    # train_label = train_data[:, 0].copy()
+    # train_feature = train_data[:, 1:65]
+    #
+    # test_label = test_data[:, 0].copy()
+    # test_feature = test_data[:, 1:65]
 
     # train_data = np.genfromtxt('E:\\DataSet\\Statlog\\train.txt', delimiter=',')
     # test_data = np.genfromtxt('E:\\DataSet\\Statlog\\test.txt', delimiter=',')
--- my_main.py
+++ my_main.py
@@ -3,4 +3,5 @@ import most_nearest_parzen_window
 import ldf_qdf
 
 #ldf_qdf.ldf_qdf_main()
-most_nearest_parzen_window.most_nearest_distance()
+# most_nearest_parzen_window.most_nearest_distance()
+mqdf.mqdf_main()
